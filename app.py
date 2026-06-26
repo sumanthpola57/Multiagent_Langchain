db02f36f357e8df7ff66bb5eb18a353145c9ec38
sumanth <sumanthpola57@gmail.com>
2026-06-26 18:03:08 +0530

    Tools websearch and scraping

---
+++ app.py
@@ -0,0 +1,3 @@
+from src.tools.tools import web_search
+websearch=("latest ai news")
+print(web_search)
---
+++ main.py
@@ -0,0 +1,4 @@
+from src.tools.tools import scrape_url
+
+result = scrape_url.invoke(" https://www.artificialintelligence-news.com")
+print(result)
